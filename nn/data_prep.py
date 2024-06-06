638618d6c69f0812a99ca5675205e17fa65af4e3
ana <anaisabelmartins@tecnico.ulisboa.pt>
2024-06-06 13:16:48 +0200

    refactor: Remove unused import in data_prep.py

--- nn/data_prep.py
+++ nn/data_prep.py
@@ -7,7 +7,6 @@ from xml.sax.handler import DTDHandler
 import numpy as np
 import pandas as pd
 import torch
-from scipy.fft import fft
 from sklearn import preprocessing
 from sklearn.model_selection import train_test_split
 from torch.utils.data import DataLoader, TensorDataset
